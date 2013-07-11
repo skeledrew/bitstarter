5f225d52e42749b37e7bee69d9632f798adab412
Andrew Phillips <skeledrew@gmail.com>
2013-07-11 08:28:52 -0500

    Fixed web.js bug 2

--- web.js
+++ web.js
@@ -5,13 +5,11 @@ var app = express.createServer(express.logger());
 var index = fs.readFileSync( "index.html" )  // get text from index file
 var buffer = new Buffer(index) // hold text
 
-console.log(buffer.toString())
-
-/*app.get('/', function(request, response) {
+app.get('/', function(request, response) {
   response.send( Buffer.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});*/
+});
