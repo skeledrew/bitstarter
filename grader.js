e391ebc068951de9eb58ac4711a9ff2a6d0afe19
Andrew Phillips <skeledrew@gmail.com>
2013-07-11 17:22:36 -0500

    Broken grader.js

--- grader.js
+++ grader.js
@@ -24,12 +24,14 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');  // for downloading URLs
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+    if(!fs.existsSync(instr) && instr.indexOf('://"') !== -1) {
+        // !!! extra check will prob explode with an invalid URL...
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
@@ -45,6 +47,18 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
+    //var savfile = makeClosure(checksfile);  // !!! Possibly problematic on recall
+    if (htmlfile.indexOf('://' !== -1)) {
+        // check if a URL was given
+        var i = 0;
+        var saveFile = makeClosure(checksfile);  // setup closure
+        restler.get(htmlfile).on('complete', saveFile);  // download...
+        console.log('Debug: ' + htmlfile);
+        while (true) {
+            // waits forever
+            i++;
+        }
+    }
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -61,10 +75,25 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var makeClosure = function(checksfile) {
+    // create closure to make checksfile available
+var saveFile = function(result, response) {
+    // save downloaded file and recall function
+    if (result instanceof Error) {
+        console.error('Error: ' + response.message);
+        process.exit(1);
+    } else {
+        fs.writeFileSync(htmlfile, result);
+        checkHtmlFile(htmlfile, checksfile);
+    }
+};
+    return saveFile;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path or URL to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
