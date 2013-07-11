8ad0beba1a19e09d03ad6aba7ae0bd57a025f58d
Andrew Phillips <skeledrew@gmail.com>
2013-07-11 08:25:49 -0500

    Fixed web.js bug

--- web.js
+++ web.js
@@ -3,13 +3,15 @@ var fs = require('fs');  // add filesystem functionality
 
 var app = express.createServer(express.logger());
 var index = fs.readFileSync( "index.html" )  // get text from index file
-var buffer = new Buffer(index, "utf-8")  // hold text
+var buffer = new Buffer(index) // hold text
 
-app.get('/', function(request, response) {
+console.log(buffer.toString())
+
+/*app.get('/', function(request, response) {
   response.send( Buffer.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});*/
