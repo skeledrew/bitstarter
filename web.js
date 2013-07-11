0656009c3fd6a004c816448e083b345c2ce8a026
Andrew Phillips <skeledrew@gmail.com>
2013-07-11 08:05:28 -0500

    Edited web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');  // add filesystem functionality
 
 var app = express.createServer(express.logger());
+var index = fs.readFileSync( "index.html" )  // get text from index file
+var buffer = new Buffer(index, "utf-8")  // hold text
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( Buffer.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
