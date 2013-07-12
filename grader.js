fae471ee0cb4dc19b56d6113701e8329244e710a
Andrew Phillips <skeledrew@gmail.com>
2013-07-11 22:27:37 -0500

    Working version of upgraded grader.js

--- grader.js
+++ grader.js
@@ -30,8 +30,7 @@ var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr) && instr.indexOf('://"') !== -1) {
-        // !!! extra check will prob explode with an invalid URL...
+    if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
@@ -46,19 +45,7 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    //var savfile = makeClosure(checksfile);  // !!! Possibly problematic on recall
-    if (htmlfile.indexOf('://' !== -1)) {
-        // check if a URL was given
-        var i = 0;
-        var saveFile = makeClosure(checksfile);  // setup closure
-        restler.get(htmlfile).on('complete', saveFile);  // download...
-        console.log('Debug: ' + htmlfile);
-        while (true) {
-            // waits forever
-            i++;
-        }
-    }
+var checkHtmlFile2 = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -66,7 +53,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    printResult(out);  // changed from return to accommodate event call
 };
 
 var clone = function(fn) {
@@ -75,29 +62,45 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-var makeClosure = function(checksfile) {
+var makeClosure = function(htmlfile, checksfile) {
     // create closure to make checksfile available
-var saveFile = function(result, response) {
-    // save downloaded file and recall function
-    if (result instanceof Error) {
-        console.error('Error: ' + response.message);
-        process.exit(1);
+    var saveFile = function(result, response) {
+        // save downloaded file and recall function
+        if (result instanceof Error) {
+            console.error('Error: ' + response.message);
+            process.exit(1);
+        } else {
+            fs.writeFileSync(htmlfile, result);
+            checkHtmlFile2(htmlfile, checksfile);
+        }
+    };
+    return saveFile;
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, indexurl) {
+    var saveFile = makeClosure(htmlfile, checksfile);
+
+    if (indexurl) {
+        restler.get(indexurl).on('complete', saveFile)
     } else {
-        fs.writeFileSync(htmlfile, result);
-        checkHtmlFile(htmlfile, checksfile);
+        assertFileExists(htmlfile);
+        checkHtmlFile2(htmlfile, checksfile);
     }
 };
-    return saveFile;
+
+var printResult = function(checkJson) {
+    // makes final output statements available from anywhere
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 };
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path or URL to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    checkHtmlFile(program.file, program.checks, program.url);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
